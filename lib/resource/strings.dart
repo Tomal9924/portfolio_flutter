66bdfea0c988dc54b1fe54e7ef9363cf470aca9b
tomal <tomalahmed350@outlook.com>
2025-04-05 00:09:44 +0600

    initial

---
+++ lib/resource/strings.dart
@@ -0,0 +1,51 @@
+class Strings {
+  /* Intro screen contents */
+  static final welcomeTxt = '''Hi, my name is''';
+  static final name = '''Tanvir Ahmed.'''; // add . at the end
+  static final whatIdo =
+      '''I build things for the mobile.'''; // add . at the end
+  static final introAbout =
+      '''I'm a software engineer with a passion for crafting exceptional digital experiences. Specializing in Flutter, I build (and sometimes design) cross-platform applications that are fast, accessible, and user-friendly. Currently, I’m focused on creating human-centered products that prioritize inclusivity, performance, and seamless interaction at ''';
+  static final currentOrgName = '''Piistech Limited.'''; // add . at the end
+
+  /* About screen contents */
+  static final aboutPara1 =
+      '''Hello! I am a passionate Software Engineer specializing in Flutter app development, with over 5 years of experience crafting high-performance, cross-platform mobile applications. I thrive on turning complex ideas into sleek, functional, and accessible digital experiences.
+
+I’ve successfully built and maintained over 10 mobile apps, integrating features like push notifications, live audio streaming, offline capabilities, and deep linking. My development approach follows Clean Architecture and SOLID principles, ensuring scalable and maintainable code.
+
+Skilled in using state management tools like Bloc, Cubit, Provider, and Riverpod, I also bring experience in backend integration using RESTful APIs, Firebase services, and tools like Agora.io and Google ML Kit.
+
+Whether it's launching polished apps on Codecanyon or developing internal enterprise tools, I bring a user-focused mindset and a commitment to quality at every step of the way. Let's collaborate and bring your ideas to life!''';
+
+  static final techIntro =
+      '''Here are a few technologies I’ve been working with recently:''';
+  static final tech1 = ''' Flutter'''; // space for manual padding
+  static final tech2 = ''' Java (Native)'''; // space for manual padding
+
+  /* Exp screen contents */
+  static final expCompName1 = '''Neat Solutions''';
+  static final expDesig1 = '''Flutter Developer (Part-time)''';
+  static final expDur1 = '''July 2021-Feb 2022''';
+  static final expAbout1 =
+      '''Developed and maintained cross-platform mobile applications using Flutter (Dart).''';
+  static final expAbout1_2 =
+      '''Implemented UI/UX components based on design mockups''';
+  static final expAbout1_3 =
+      '''Integrated Google Maps API for real-time ride tracking and location sharing''';
+
+  static final expCompName2 = '''Piistech Limited''';
+  static final expDesig2 = '''Software Engineer (Flutter) ''';
+  static final expDur2 = '''Jan 2019 - Present''';
+  static final expAbout2 =
+      '''Developed and maintained 10+ mobile applications using Flutter, effectively reducing cross-platform development time.''';
+  static final expAbout2_2 =
+      '''Supported senior developers in integrating APIs and payment gateways, enhancing overall functionality and user experience.''';
+  static final expAbout2_3 =
+      '''Developed and launched multiple apps on Codecanyon, adhering to SOLID principles for maintainable
+and scalable code.''';
+
+  /* Contact screen content */
+  static final endTxt =
+      '''Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!''';
+}
